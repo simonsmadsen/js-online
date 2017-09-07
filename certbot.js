ddff3618cea368936c649ef523b1dee8fe19bf0e
simonsmadsen <simon@smadsen.com>
2017-09-07 14:11:09 +0200

    nginx

--- certbot.js
+++ certbot.js
@@ -58,9 +58,9 @@ const ensureCert = run => async program => {
   console.log(certs)
   const line = certlistMatch(program,certs)
   console.log(line)
-  const hasCert = !line || line.length < 1
+  const noCert = !line || line.length < 1
   console.log(hasCert);
-  if(!hasCert){
+  if(!noCert){
      const r = await run(newCert(program))
      console.log(r)
   }
