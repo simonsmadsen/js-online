3314c75764e220509d4659656fa34c0bfa34f213
simonsmadsen <simon@smadsen.com>
2017-09-07 13:43:04 +0200

    nginx

--- program-utils.js
+++ program-utils.js
@@ -1,6 +1,6 @@
 module.exports = {
   programENV: program => 'websites/'+program.domain+'/.env',
-  programInstall: program => 'cd websites/'+program.domain+'/ && npm install',
+  programInstall: program => 'cd websites/'+program.domain+'/ && yarn install',
   programStart: program => 'cd websites/'+program.domain+'/ && pm2 start app.js --name "'+program.domain+'" ',
   programRestart: program => 'cd websites/'+program.domain+'/ && pm2 restart "'+program.domain+'" ',
   programPath: program => 'websites/'+program.domain+'/'
