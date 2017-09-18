9afa538603fcc9b3e0dfcd511ab8015e11c506d7
simonsmadsen <simon@smadsen.com>
2017-09-18 15:44:30 +0200

    migrations

--- program-utils.js
+++ program-utils.js
@@ -1,7 +1,7 @@
 module.exports = {
   programENV: program => 'websites/'+program.domain+'/.env',
   programInstall: program => 'cd websites/'+program.domain+'/ && yarn install',
-  programStart: program => 'cd websites/'+program.domain+'/ && pm2 start app.js --name "'+program.domain+'" ',
+  programStart: program => 'cd websites/'+program.domain+'/ && pm2 start '+program.file+' --name "'+program.domain+'" ',
   migrate: program => 'cd websites/'+program.domain+'/ && node '+program.migration,
   programRestart: program => 'cd websites/'+program.domain+'/ && pm2 restart "'+program.domain+'" ',
   programPath: program => 'websites/'+program.domain+'/'
